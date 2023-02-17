6580dcb39f6c275567b2c3f3d42ad3dc246a7af7
Eduarda Schüler Costa <eduarda.schuler.costa@gmail.com>
2023-02-17 16:13:37 -0300

    Create Filme.py

---
+++ Filme.py
@@ -0,0 +1,4 @@
+from Midias_audiovisuais import Midias_audiovisuais
+class Filme(Midias_audiovisuais):
+    def __init__(self, pTitulo, pAno, pRoteirista, pDiretor, pGenero):
+        super().__init__(pTitulo, pAno, pRoteirista, pDiretor, pGenero)
