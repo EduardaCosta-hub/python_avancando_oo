ee20cc5328692a1c4d2c9ec41e23218707e9bb0a
Eduarda Schüler Costa <eduarda.schuler.costa@gmail.com>
2023-02-20 11:44:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from Serie import Serie
 from Playlist import Playlist
 filme1 = Filme("Titulo generico",2023,"Fulano","Siclano","Comédia Romantica",100)
 serie1 = Serie("Uma série",2023,"Roteiro","Direção","Comédia",4)
+serie1.dar_like()
+filme1.dar_like()
+filme1.dar_like()
 programas = [filme1, serie1]
 playlist1 = Playlist("Playlist de Teste", programas)
 print(f"Tamanho da Playlist: {len(playlist1)}")
